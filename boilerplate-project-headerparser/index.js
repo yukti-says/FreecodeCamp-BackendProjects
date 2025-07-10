a72e0988afcf59e94682da4cb6e728999a97ee37
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-07-10 07:55:17 +0530

    Add /api/whoami endpoint for header parser microservice

--- boilerplate-project-headerparser/index.js
+++ boilerplate-project-headerparser/index.js
@@ -21,8 +21,17 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+  res.json({ greeting: 'hello API yukti' });
 });
+
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
